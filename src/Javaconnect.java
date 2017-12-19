b8e63a326fd7b243fba2637bcb8937a7ea0c2c16
osama823 <osama823@gmail.com>
2017-12-19 06:13:56 -0800

    first commit

---
+++ src/Javaconnect.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author OK COMPUTER
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Javaconnect {
+    
+    Connection conn;
+    public static Connection conncetDb(){
+    
+    try{
+    Class.forName("org.sqlite.JDBC");
+    Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\OK COMPUTER\\Documents\\NetBeansProjects\\Library Managment System\\LibraryDb.sqlite");
+    return conn;
+    }
+    catch(Exception e){
+    JOptionPane.showMessageDialog(null, e);
+    return null;
+    
+    }
+    
+    
+    }
+    }
+
